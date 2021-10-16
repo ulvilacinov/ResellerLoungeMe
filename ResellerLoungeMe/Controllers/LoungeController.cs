4676604864cb91a4390eaa8e458537d9496fab34
UlviLacinov <ulvilacinov@gmail.com>
2021-10-16 15:20:40 +0300

    created lounge detail page

---
+++ ResellerLoungeMe/Controllers/LoungeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResellerLoungeMe.Data.APIs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResellerLoungeMe.Controllers
+{
+    public class LoungeController : Controller
+    {
+        LoungeAdapter loungeAdapter = new LoungeAdapter();
+        public IActionResult Index(int id)
+        {
+            var result = loungeAdapter.GetLounge(id);
+            if (result.Id == 0)
+            {
+                return View("NotFound");
+            }
+            return View(result);
+        }
+    }
+}
